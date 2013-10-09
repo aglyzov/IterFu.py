424e33769ddc76c02bc340e3cd6f4583bb587794
Alexander Glyzov <bonoba@gmail.com>
2013-10-10 00:22:04 +0300

    added setup.py and MANIFEST files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name         = 'IterFu',
+    version      = '0.1',
+    description  = 'Iterator KungFu -- a library of handy iterator functions',
+    author       = 'Alexander Glyzov',
+    author_email = 'bonoba@gmail.com',
+    url          = 'https://github.com/aglyzov/IterFu.py',
+    py_modules   = ['iterfu'],
+)
